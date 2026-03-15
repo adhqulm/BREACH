f4cd5b709ce83200ee600c51399122ce5ae502f5
Kristina Ganina <adhqulm@gmail.com>
2026-03-15 14:53:08 +0400

    mac

--- electron/main.cjs
+++ electron/main.cjs
@@ -34,3 +34,7 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
